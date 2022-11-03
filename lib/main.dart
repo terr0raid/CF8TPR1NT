418e9bb3e254d74a1bf72f5987dff277d5ae5837
cyclnxd <herakles13579@gmail.com>
2022-11-03 19:27:33 +0300

    added firebase initialize

--- lib/main.dart
+++ lib/main.dart
@@ -5,9 +5,11 @@ import 'package:cf8tpr1nt/core/init/navigation/navigation_route.dart';
 import 'package:cf8tpr1nt/core/init/navigation/navigation_service.dart';
 import 'package:cf8tpr1nt/core/init/provider/application_provider.dart';
 import 'package:cf8tpr1nt/core/init/provider/theme_provider.dart';
-import 'package:cf8tpr1nt/view/authentication/onboard/view/onboard_view.dart';
+import 'package:cf8tpr1nt/firebase_options.dart';
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 Future<void> main() async {
@@ -29,9 +31,14 @@ Future<void> main() async {
 
 Future<void> _init() async {
   WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initializeApp(
-  //   options: DefaultFirebaseOptions.currentPlatform,
-  // );
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  } catch (e) {
+    await SystemNavigator.pop();
+  }
+
   await EasyLocalization.ensureInitialized();
   await LocaleManager.preferencesInstance();
 }
@@ -42,7 +49,6 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const OnBoardView(),
       debugShowCheckedModeBanner: false,
       theme: context.watch<ThemeProvider>().currentTheme,
       localizationsDelegates: context.localizationDelegates,
